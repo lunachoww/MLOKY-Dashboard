2a5de4846194d147bfbffbe4088eecc0d8044d3a
Sajib Hasan <sajib001@gmail.com>
2022-02-24 02:56:25 +0600

    Fix vite docker config

--- Dockerfile
+++ Dockerfile
@@ -6,19 +6,13 @@ RUN apk add --no-cache git openssh
 # set working directory
 WORKDIR /app
 
-# add `/app/node_modules/.bin` to $PATH
-ENV PATH /app/node_modules/.bin:$PATH
-
 # install app dependencies
 COPY package.json ./
 COPY yarn.lock ./
 RUN yarn
-RUN yarn add react-scripts@3.4.1 -g --silent
 
-# add app
+# copy app
 COPY . ./
-
-EXPOSE 3000
 
 # start app
 CMD ["yarn", "dev"]
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "./node_modules/@esbuild-plugins/node-globals-polyfill/esm/index";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		host: "0.0.0.0",
+		port: 3000,
+	},
 	define: {
 		"process.env": process.env,
 		// global: "globalThis",
 	},
-	plugins: [react()],
+	plugins: [react(), reactRefresh()],
 	optimizeDeps: {
 		esbuildOptions: {
 			// Node.js global to browser globalThis
